a47a5274f63685550366155dc822bcb132260751
Jake Craige <jake@poeticsystems.com>
2014-11-14 12:53:04 -0600

    test the middleware

--- tests/unit/middleware-test.js
+++ tests/unit/middleware-test.js
@@ -1,6 +1,6 @@
-var fs   = require('fs');
-var path = require('path');
-var tmp  = require('tmp-sync');
+var fs      = require('fs');
+var path    = require('path');
+var tmp     = require('tmp-sync');
 
 var middleware = require('../../lib/middleware');
 
@@ -9,10 +9,7 @@ describe('Middleware', function() {
     this.tmpDir = tmp.in(__dirname);
     tmp.mark(this.tmpDir);
 
-    this.options = {
-      srcDir: this.tmpDir
-    };
-
+    this.options = { srcDir: this.tmpDir };
 
     this.readFixture = function(/* path, to file */) {
       var args = [].slice.call(arguments);
@@ -23,71 +20,63 @@ describe('Middleware', function() {
         encoding: 'utf8'
       }));
     }
+
+    // This is all the
+    this.proxyRequest = function(options) {
+      var res = {
+        end: noop, write: noop, _headers: {},
+        req: { method: options.method, url: options.url },
+        statusCode: options.statusCode
+      };
+      this.server({headers: {}}, res, noop);
+      res.write.call({
+        headers: options.headers
+      }, JSON.stringify(options.body));
+    };
+
+    this.writeFixtures = function(body) {
+      this.server({
+        method: 'POST',
+        url: '/write-fixtures',
+        body: body || [],
+      }, {end: noop}, noop);
+    }
+
+    this.clearFixtures = function() {
+      this.server({
+        method: 'DELETE',
+        url: '/clear-fixtures'
+      }, {end: noop}, noop);
+    }
+  });
+
+  beforeEach(function() {
+    this.server = middleware(this.options);
   });
 
   describe('POST /write-fixtures', function() {
-    beforeEach(function() {
-      middleware(this.options)({
-        method: 'POST',
-        url: '/write-fixtures',
-        body: [
-          {
-            reqHeaders: {
-              'x-module-name': 'test', 'x-test-name': 'test-get'
-            },
-            method: 'GET',
-            url: '/users',
-            statusCode: 200,
-            headers: {},
-            body: { users: [{id: 0}] }
-          },
-          {
-            reqHeaders: {
-              'x-module-name': 'test', 'x-test-name': 'test-get'
-            },
-            method: 'GET',
-            url: '/users?limit=0',
-            statusCode: 200,
-            headers: {},
-            body: { users: [] }
-          },
-          {
-            reqHeaders: {
-              'x-module-name': 'test', 'x-test-name': 'test-post'
-            },
-            method: 'POST',
-            url: '/users',
-            statusCode: 201,
-            headers: { Authorization: 'Bearer 12345' },
-            body: { user: { id: 0 } }
-          },
-          {
-            reqHeaders: {
-              'x-module-name': 'test', 'x-test-name': 'test-put'
-            },
-            method: 'PUT',
-            url: '/users/0',
-            statusCode: 204,
-            headers: { Authorization: 'Bearer 12345' },
-            body: {}
-          },
-          {
-            reqHeaders: {
-              'x-module-name': 'test', 'x-test-name': 'test-delete'
-            },
-            method: 'DELETE',
-            url: '/users/0',
-            statusCode: 204,
-            headers: { Authorization: 'Bearer 12345' },
-            body: {}
-          }
-        ]
-      }, {end: function() {}}, function() {});
-    });
-
     describe('writes fixtures', function() {
       describe('GET', function() {
         beforeEach(function() {
+          this.writeFixtures([
+            {
+              reqHeaders: { 'x-module-name': 'test', 'x-test-name': 'test-get' },
+              method: 'GET',
+              url: '/users',
+              statusCode: 200,
+              headers: {},
+              body: { users: [{id: 0}] }
+            },
+            {
+              reqHeaders: { 'x-module-name': 'test', 'x-test-name': 'test-get' },
+              method: 'GET',
+              url: '/users?limit=0',
+              statusCode: 200,
+              headers: {},
+              body: { users: [] }
+            }
+          ]);
+
           this.fixture = this.readFixture('test', 'test-get.json')
         });
 
@@ -118,6 +107,15 @@ describe('Middleware', function() {
 
       describe('POST', function() {
         beforeEach(function() {
+          this.writeFixtures([{
+            reqHeaders: { 'x-module-name': 'test', 'x-test-name': 'test-post' },
+            method: 'POST',
+            url: '/users',
+            statusCode: 201,
+            headers: { Authorization: 'Bearer 12345' },
+            body: { user: { id: 0 } }
+          }]);
+
           this.fixture = this.readFixture('test', 'test-post.json')
         });
 
@@ -137,6 +135,15 @@ describe('Middleware', function() {
 
       describe('PUT', function() {
         beforeEach(function() {
+          this.writeFixtures([{
+            reqHeaders: { 'x-module-name': 'test', 'x-test-name': 'test-put' },
+            method: 'PUT',
+            url: '/users/0',
+            statusCode: 204,
+            headers: { Authorization: 'Bearer 12345' },
+            body: {}
+          }]);
+
           this.fixture = this.readFixture('test', 'test-put.json')
         });
 
@@ -156,6 +163,15 @@ describe('Middleware', function() {
 
       describe('DELETE', function() {
         beforeEach(function() {
+          this.writeFixtures([{
+            reqHeaders: { 'x-module-name': 'test', 'x-test-name': 'test-delete' },
+            method: 'DELETE',
+            url: '/users/0',
+            statusCode: 204,
+            headers: { Authorization: 'Bearer 12345' },
+            body: {}
+          }]);
+
           this.fixture = this.readFixture('test', 'test-delete.json')
         });
 
@@ -176,10 +192,43 @@ describe('Middleware', function() {
   });
 
   describe('DELETE /clear-fixtures', function() {
-    it('clears fixtures');
+    it('clears fixtures', function() {
+      this.proxyRequest({
+        method: 'GET', url: '/users', statusCode: 200,
+        headers: { 'x-module-name': 'test', 'x-test-name': 'test-clear' },
+        body: {users: []}
+      });
+      this.clearFixtures();
+      this.writeFixtures();
+
+      var fixturePath = path.join(this.tmpDir, 'test', 'test-clear.json');
+      assert(!fs.existsSync(fixturePath), 'test-clear fixture should not be created');
+    });
   });
-  describe('hooks into res.write', function() {
-    it('adds fixture before writing response');
+
+  describe('proxy request', function() {
+    beforeEach(function() {
+      this.proxyRequest({
+        method: 'GET',
+        url: '/users',
+        statusCode: 200,
+        headers: { 'x-module-name': 'test', 'x-test-name': 'test-proxy' },
+        body: {users: []}
+      });
+      this.writeFixtures();
+    });
+
+    it('hooks into res.write to intercept proxied requests', function() {
+      var fixture = this.readFixture('test', 'test-proxy.json')['/users']['get'][''].fixtures[0];
+
+      assert.deepEqual(Object.keys(fixture),
+                      ['statusCode', 'headers', 'body']);
+
+      assert.equal(fixture.statusCode, 200);
+      assert.equal(fixture.body, '{"users":[]}');
+      assert.deepEqual(fixture.headers, {});
+    });
   });
 });
 
+function noop() { return this; }
